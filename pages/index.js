21c93d995901087b3a4c578285924d0123b9729d
Andreas Bigger <bigger@usc.edu>
2020-05-31 18:49:13 -0400

    Description

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ const Home = () => (
       <h1 className="title">Fast Feedback</h1>
 
       <p className="description">
-        Get started by editing <code>pages/index.js</code>
+        A website to create single line commenting on any website.
       </p>
 
       <div className="grid">
